04e88b1b540862051eba95da884469b8d55a75a6
joshcv2112 <joshcv2112@gmail.com>
2021-01-09 19:16:18 -0800

    re-adding stuff

---
+++ CookbookAPI/Controllers/RecipesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookbookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        private readonly RecipeRepository recipeRepository;
+        public RecipesController()
+        {
+            recipeRepository = new RecipeRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Recipe> Get()
+        {
+            return recipeRepository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public Recipe Get(int id)
+        {
+            return recipeRepository.GetById(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Recipe prod)
+        {
+            if (ModelState.IsValid)
+                recipeRepository.Add(prod);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Recipe prod)
+        {
+            prod.RecipeId = id;
+            if (ModelState.IsValid)
+                recipeRepository.Update(prod);
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            recipeRepository.Delete(id);
+            // Add call to delete all notes for a recipe.
+        }
+    }
+}
---
+++ CookbookAPI/Models/RecipeRepository.cs
@@ -0,0 +1,77 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookbookAPI.Models
+{
+    public class RecipeRepository
+    {
+        private string connectionString;
+        public RecipeRepository()
+        {
+            connectionString = @"Persist Security Info=False;User ID=CookbookAPI;Password=password;Server=LAPTOP-3R0QQVMN;Initial Catalog=CookbookDB;";
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(connectionString);
+            }
+        }
+
+        public void Add(Recipe rcp)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"INSERT INTO Recipes(Title, Ingredients, Instructions, [Description], Source, Rating, PrepTime, ImageURL, SectionId, CookbookId) VALUES(@Title, @Ingredients, @Instructions, @Description, @Source, @Rating, @PrepTime, @ImageURL, @SectionId, @CookbookId);";
+                dbConnection.Open();
+                dbConnection.Execute(sQuery, rcp);
+            }
+        }
+
+        public IEnumerable<Recipe> GetAll()
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"SELECT * FROM Recipes";
+                dbConnection.Open();
+                return dbConnection.Query<Recipe>(sQuery);
+            }
+        }
+
+        public Recipe GetById(int id)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"SELECT * FROM Recipes WHERE Id=@Id";
+                dbConnection.Open();
+                return dbConnection.Query<Recipe>(sQuery, new { RecipeId = id }).FirstOrDefault();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"DELETE FROM Recipes WHERE Id=@Id";
+                dbConnection.Open();
+                dbConnection.Execute(sQuery, new { RecipeId = id });
+            }
+        }
+
+        public void Update(Recipe prod)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"UPDATE Recipes SET Title=@Title, Ingredients=@Ingredients, Instructions=@Instructions, [Description]=@Description, Source=@Source, Rating=@Rating, PrepTime=@PrepTime, ImageURL=@ImageURL, SectionId=@SectionId, CookbookId=@CookbookId WHERE RecipeID=@RecipeID;";
+                dbConnection.Open();
+                dbConnection.Query(sQuery, prod);
+            }
+        }
+    }
+}
